eaa7ce45e91fadc18e5744b3ce187a773f893f37
Samin1615088 <samin.shafkat@gmail.com>
2021-06-09 15:19:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,92 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-4 font-nunito">
+      {/* nav start */}
+      <nav className="col-span-1">
+        <div>
+          <h1>
+            <a href="/">Food Ninja</a>
+            <a href='}'>LinkedIn handle</a>
+          </h1>
+        </div>
+
+        <ul>
+          <li>
+            <a href="#">
+              <span>Home</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <span>About</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <span>Contact</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/* nav end */}
+
+      <main className="col-span-3">
+        <div>
+          <a href="#"
+            className=""
+          >Log in</a>
+          <a href="#">Sign up</a>
+        </div>
+
+        <header>
+          <h2 >Recipes</h2>
+          <h3>For Ninjas</h3>
+        </header>
+
+        <div>
+          <h4 className="text-primary ">Latest Recipes</h4>
+
+          <div className="grid grid-cols-3 gap-3">
+            {/* card go here */}
+            <div className="card">
+              <img className="object-cover w-full" src="img/stew.jpg" alt="" />
+              <div className="m-3">
+                <span className="block font-bold text-gray-600 text-lg">5 Bean Chi</span>
+                <span className="block font-light text-sm">Recipe by Mario</span>
+              </div>
+            </div>
+
+            <div className="card">
+              <img className="object-cover w-full" src="img/stew.jpg" alt="" />
+              <div className="m-3">
+                <span className="block font-bold text-gray-600 text-lg">5 Bean Chi</span>
+                <span className="block font-light text-sm">Recipe by Mario</span>
+              </div>
+            </div>
+
+            <div className="card">
+              <img className="object-cover w-full" src="img/stew.jpg" alt="" />
+              <div className="m-3">
+                <span className="block font-bold text-gray-600 text-lg">5 Bean Chi</span>
+                <span className="block font-light text-sm">Recipe by Mario</span>
+              </div>
+            </div>
+          </div>
+
+          <h4>Latest Recipes</h4>
+          <div>
+            {/* cards go here */}
+          </div>
+
+          <div>
+            <div>Load  more</div>
+          </div>
+        </div>
+
+      </main>
     </div>
   );
 }
